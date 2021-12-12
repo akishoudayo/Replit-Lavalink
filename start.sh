f4ab49bcba46ecfcb829bd15b72b96a3d8fd928c
あきしょう <akishoudayo@gmail.com>
2021-12-12 13:44:18 +0900

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,2 @@
+wget -c -nc https://github.com/freyacodes/Lavalink/releases/download/3.4/Lavalink.jar
+java -jar Lavalink.jar -XX:+UseContainerSupport -Xmx2G -Xss512k -XX:CICompilerCount=4 -Dfile.encoding=UTF-8 
